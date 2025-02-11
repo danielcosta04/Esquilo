7a5cf7b616e6d4088293e1e38d87b89ee1cf194b
daniel costa <dalpcosta@gmail.com>
2025-02-11 14:54:07 -0300

    teste

--- esquilo.js
+++ esquilo.js
@@ -1,23 +1,35 @@
+// esquilo.js
+let hamsterSpeedFactor = 4;
+
 function init() {
     const wrapper = document.querySelector(".wrapper");
     const wheel = document.querySelector(".wheel");
-    const defaultHamsterEnergy = 1000;
     const hamster = {
-        energy: defaultHamsterEnergy,
-        speedFactor: 4,
-        isRunning: true // Corrigido "isrunning" para "isRunning"
+        speedFactor: hamsterSpeedFactor
     };
 
     const setSpeed = () => {
         wrapper.style.setProperty("--hamster-speed",
             `${1 / hamster.speedFactor}s`);
         wrapper.style.setProperty("--wheel-speed",
-            `${2 / hamster.speedFactor}s`); // Corrigido de "{$ 2 / hamster.speedFactor}s" para `${2 / hamster.speedFactor}s`
-        wrapper.style.setProperty("--wheel-angle",
-            `${0.4 * hamster.speedFactor}deg`);
+            `${2 / hamster.speedFactor}s`);
     };
 
-    setSpeed(); // Chamando a função para aplicar as mudanças
+    setSpeed();
+}
+
+function increaseSpeed() {
+    if (hamsterSpeedFactor > 1) {
+        hamsterSpeedFactor--;
+        init();
+    }
+}
+
+function decreaseSpeed() {
+    if (hamsterSpeedFactor < 10) {
+        hamsterSpeedFactor++;
+        init();
+    }
 }
 
 document.addEventListener("DOMContentLoaded", init);
